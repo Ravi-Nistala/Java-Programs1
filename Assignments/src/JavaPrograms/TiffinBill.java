f214df7e15e5b61583ccf486e43a31f082bb85ec
NistalaR <NistalaR@HBADT4950207.bsg.ad.adp.com>
2021-04-09 21:38:02 +0530

    Tiffin

--- Assignments/src/JavaPrograms/TiffinBill.java
+++ Assignments/src/JavaPrograms/TiffinBill.java
@@ -5,6 +5,19 @@ public class TiffinBill {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc=new Scanner(System.in);
+		int i,n,count;
+		System.out.println("Enter Item number (1.Dosa, 2.Idly, 3.Pongal): ");
+		i=sc.nextInt();
+		System.out.println("Number of plates: ");
+		n=sc.nextInt();
+		if(i==1)
+			count=20*n;
+		else if(i==2)
+			count=10*n;
+		else
+			count=30*n;
+		System.out.println("Final bill: "+count);
 	}
 
 }
