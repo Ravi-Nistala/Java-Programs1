9702796bca2aafeb2e4f1756ed0f3fec63ab3d66
NistalaR <NistalaR@HBADT4950207.bsg.ad.adp.com>
2021-04-09 21:34:50 +0530

    Seasons

--- Assignments/src/JavaPrograms/Seasons.java
+++ Assignments/src/JavaPrograms/Seasons.java
@@ -1,10 +1,24 @@
 package JavaPrograms;
 
+import java.util.Scanner;
+
 public class Seasons {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc=new Scanner(System.in);
+		int i;
+		System.out.println("Enter Month: ");
+		i=sc.nextInt();
+		if(i>=1 && i<=4)
+			System.out.println("Given month belongs to Summer Season");
+		else if(i>=5 && i<=8)
+			System.out.println("Given month belongs to Winter Season");
+		else if (i>=9 && i<=12)
+			System.out.println("Given month belongs to Rainy Season");
+		else
+			System.out.println("Enter valid month");
 	}
 
 }
